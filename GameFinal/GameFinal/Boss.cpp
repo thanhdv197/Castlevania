28b7bd99cfa5bf1ab01cf262406f64f4eac3b7f7
Thanh <duongvanthanh200@gmail.com>
2020-06-01 16:43:42 +0700

    fix boss

--- GameFinal/GameFinal/Boss.cpp
+++ GameFinal/GameFinal/Boss.cpp
@@ -45,42 +45,54 @@ void CBoss::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 	CGameObject::Update(dt, coObjects);
 
 	// set distance to change state of boss
-	if ((this->xSimon - this->x) > 50)
+	if ((this->xSimon - this->x) > 40)
 	{
 		SetState(STATE_BOSS_FLY);
-		timeChangeState += dt;
-
-		// set time to change waitting state
-		if (timeChangeState > 3000)
-		{
-			if (this->state == STATE_BOSS_FLY)
-			{
-				SetState(STATE_BOSS_FLY_WAIT);
-			}
-		}
 	}
-	// set time to change fly state from waitting state
-	if (this->state == STATE_BOSS_FLY_WAIT)
+
+	if (this->state != STATE_BOSS_STAND)
 	{
 		timeChangeState += dt;
-		if (timeChangeState > 7000)
+	}
+
+	if (this->state == STATE_BOSS_FLY)
+	{
+		// set time to change waitting state
+		if (timeChangeState > 2000)
 		{
-			SetState(STATE_BOSS_FLY);
+			SetState(STATE_BOSS_FLY_WAIT);
 			timeChangeState = 0;
 		}
 	}
+	else if (this->state == STATE_BOSS_FLY_WAIT)
+	{
+		if (timeChangeState > 2000)
+		{
+			// set nx of bird
+			if (this->xSimon < this->x)
+				nx = -1;
+			else
+				nx = 1;
 
-	// set nx of bird
-	if (this->xSimon - this->x > 30)
-		nx = 1;
-	else if (this->xSimon - this->x < -30)
-		nx = -1;
+			// set ny of bird
+			if (ySimon > y)
+				ny = 1;
+			else
+				ny = -1;
 
-	// set ny of bird
-	if (ySimon - y > 50)
-		ny = 1;
-	else if (ySimon - y < -10)
-		ny = -1;
+			SetState(STATE_BOSS_ATTACK);
+
+			timeChangeState = 0;
+		}
+	}
+	else if (this->state == STATE_BOSS_ATTACK)
+	{
+		if (timeChangeState > 2000)
+		{
+			SetState(STATE_BOSS_FLY_WAIT);
+			timeChangeState = 0;
+		}
+	}
 
 	// check whip attack
 	if (isAttacked)
@@ -145,15 +157,14 @@ void CBoss::SetState(int state)
 	case STATE_BOSS_FLY:
 		if (nx > 0)
 		{
-			vx = 0.03f;
+			vx = 0.05f;
 		}
-		else vx = -0.03f;
+		else vx = -0.05f;
 
 		if (ny > 0)
-			vy = 0.02f;
+			vy = 0.03f;
 		else
-			vy = -0.02f;
-
+			vy = -0.03f;
 		break;
 	case STATE_BOSS_FLY_WAIT:
 		vx = 0;
@@ -162,6 +173,18 @@ void CBoss::SetState(int state)
 	case STATE_BOSS_DIE:
 		vy = 0;
 		vx = 0;
+		break;
+	case STATE_BOSS_ATTACK:
+		if (nx > 0)
+		{
+			vx = 0.1f;
+		}
+		else vx = -0.1f;
+
+		if (ny > 0)
+			vy = 0;
+		else
+			vy = -0;
 		break;
 	default:
 		break;
